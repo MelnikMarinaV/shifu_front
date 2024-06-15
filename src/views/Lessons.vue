ee2a73fbdd1a8786b51bcda08428f9ea7ff2f4fa
Marina-mv <marina.melnik.0202@mail.ru>
2024-06-15 19:47:27 +0300

    getting and styling lessons from backend

--- src/views/Lessons.vue
+++ src/views/Lessons.vue
@@ -1,20 +1,29 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-
 import { ref, onMounted } from 'vue';
 
 const courses = ref([]);
+const lessons = ref([]);
 
 onMounted(async () => {
   try {
-    const response = await fetch('http://localhost:8001/api/courses/');
-    console.log(response)
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+    const courseResponse = await fetch('http://localhost:8001/api/courses/');
+    if (!courseResponse.ok) {
+      throw new Error('Network courseResponse was not ok');
     }
-    const data = await response.json();
-    console.log(data.courses[0].title);
-    courses.value = data.courses;
+    const courseData = await courseResponse.json();
+    courses.value = courseData.courses;
+
+    if(courses.value.length>0){
+        const courseId = courses.value[0].id;
+        const lessonsResponse = await fetch(`http://localhost:8001/api/lessons/${courseId}`);
+        
+        if (!lessonsResponse.ok) {
+        throw new Error('Network response was not ok for lessons');
+        }
+        const lessonsData = await lessonsResponse.json();
+        lessons.value = lessonsData.lessons; 
+    }
   } catch (error) {
     console.error('Error fetching items', error);
   }
@@ -27,12 +36,16 @@ onMounted(async () => {
       <div class="topnav">
         <img class="logo" src="../pictures/logo-no-background.png" alt="">
       </div>
-      <h1>Курсы</h1>
-    <ul>
-      <li v-for="course in courses" :key="course.id">
+      <h1 v-for="course in courses" :key="course.id">
         {{ course.title }} 
-      </li>
-    </ul>
+      </h1>
+      <div class ="lessons">
+        <ol v-if="lessons.length">
+            <li v-for="lesson in lessons">
+            <RouterLink :to="`/lesson/${lesson.id}`" class="router-link">{{ lesson.title }}</RouterLink>
+            </li>
+        </ol>
+      </div>
     </div>
     </body>
   </template>
@@ -44,16 +57,75 @@ onMounted(async () => {
   border: 0;
   outline: 0;
 }
-  .lessons-container{
+.lessons-container{
     min-height: 100vh;
     margin: 0;
     background: #CD071E;
-  }
-  .topnav{
+    text-align: center;
+}
+.topnav{
     padding: 5vh;
+    text-align: left;
   }
-  .logo{
+.logo{
     width: 10%;
-  }
+}
+
+h1{
+    margin-bottom: 20px;
+    font-size: 3rem;
+    font-weight: 700;
+    letter-spacing: .5rem;
+    font-family: 'Times New Roman', Times, serif;
+    background: rgb(221,229,142);
+    background: linear-gradient(0deg, rgba(221,229,142,1) 0%, rgba(224,229,177,1) 21%, rgba(182,181,48,0.8716620437237395) 100%);
+    background-clip: text;
+    color: transparent;
+}
+
+.lessons{
+  margin: auto;
+  width: 50%;
+  height: 80%;
+}
+
+ol {
+  counter-reset: section;
+  list-style-type:"HSK1 - ";
+  list-style-position: inside;
+  width: 100%;      
+  margin: 0 auto; 
+  color: #CD071E;
+  text-align: center;
+  font-size: large;
+}
+
+li::before{
+  counter-increment: section;
+  content: counters(section,".") " ";
+  color: #CD071E;
+}
+
+li{
+  background: rgb(209,131,117);
+  background: radial-gradient(circle, rgba(209,131,117,0.8744631641719187) 0%, rgba(235,237,210,0.7232026599702381) 50%); 
+  width: 100%;
+  border: 1px solid #CD071E;
+  border-radius: 25px;
+  margin-bottom: 5px;
+  padding: 10px 0 0 0;
+}
+
+li:hover{
+  background-color: gold;
+}
+
+.router-link{
+  display: block;
+  text-decoration: none;
+  color: black;
+  padding: 20px;
+  font-size: large;
+}
   </style>
   
