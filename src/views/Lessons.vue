83a9a702fd23392be0f0e4efb9767c690c3f27df
Marina-mv <marina.melnik.0202@mail.ru>
2024-06-15 15:55:33 +0300

    getting courses from back

--- src/views/Lessons.vue
+++ src/views/Lessons.vue
@@ -1,14 +1,49 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+import { ref, onMounted } from 'vue';
+
+const courses = ref([]);
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost:8001/api/courses/');
+    console.log(response)
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data.courses[0].title);
+    courses.value = data.courses;
+  } catch (error) {
+    console.error('Error fetching items', error);
+  }
+});
+</script>
+
 <template>
     <body>
     <div class = "lessons-container">
       <div class="topnav">
         <img class="logo" src="../pictures/logo-no-background.png" alt="">
       </div>
+      <h1>Курсы</h1>
+    <ul>
+      <li v-for="course in courses" :key="course.id">
+        {{ course.title }} 
+      </li>
+    </ul>
     </div>
     </body>
   </template>
   
-  <style scoped>
+  <style>
+  * {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: 0;
+}
   .lessons-container{
     min-height: 100vh;
     margin: 0;
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -12,5 +11,16 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  // конфигурация сервера разработки:
+  server: {
+    port: 5174, // Порт для фронтенда
+    proxy: {
+      '/api/': {  // Прокси для запросов к бэкенду
+        target: 'http://localhost:8001', // Адрес бэкенда
+        changeOrigin: true, 
+      }
+    }
+  } 
 })
+
