082e0cdd5488ceb9661f277fb007354bd145e7af
Marina-mv <marina.melnik.0202@mail.ru>
2024-06-15 03:14:59 +0300

    adding lessons page

---
+++ src/views/Lessons.vue
@@ -0,0 +1,24 @@
+<template>
+    <body>
+    <div class = "lessons-container">
+      <div class="topnav">
+        <img class="logo" src="../pictures/logo-no-background.png" alt="">
+      </div>
+    </div>
+    </body>
+  </template>
+  
+  <style scoped>
+  .lessons-container{
+    min-height: 100vh;
+    margin: 0;
+    background: #CD071E;
+  }
+  .topnav{
+    padding: 5vh;
+  }
+  .logo{
+    width: 10%;
+  }
+  </style>
+  
